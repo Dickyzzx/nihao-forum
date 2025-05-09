5ab279711ab2ebaad37b3835df3018985aa27832
Dicky <dickyzzx@gmail.com>
2025-05-09 17:00:07 -0400

    邀请码注册（完成）

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 # accounts/urls.py
-
+from .views import generate_invite_code
 from django.urls import path
 from . import views
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),  # ← 添加这一行
     path('send_code/', views.send_verification_code, name='send_code'),
-
-]
+    path('generate_invite/', generate_invite_code, name='generate_invite'), 
+    path('profile/', views.profile_view, name='profile'),
+    ]
